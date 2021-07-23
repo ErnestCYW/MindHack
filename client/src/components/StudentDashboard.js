91eb2cd25fad26eb32b02c84a4ce28a914d320f8
ErnestCYW <ernestcyw99@gmail.com>
2021-07-23 19:55:00 +0800

    added student dashboard

--- client/src/components/StudentDashboard.js
+++ client/src/components/StudentDashboard.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { toast } from "react-toastify";
 
 function StudentDashboard({ setAuth }) {
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState("");
+
+  const getAll = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/studentDashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token }, //from middleware
+      });
+
+      const parseRes = await response.json();
+      const messages = JSON.parse(parseRes.messages);
+
+      setName(parseRes.user_name);
+      setMessages(parseRes.messages)
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   const logout = (e) => {
     e.preventDefault();
     console.log("Triggered");
@@ -9,8 +29,23 @@ function StudentDashboard({ setAuth }) {
     setAuth(false);
     toast.success("Logged out successfully");
   };
+
+  useEffect( () => {
+    getAll();
+  }, []);
+
+  console.log(messages);
+
   return (
     <div>
+      <div className="display-1">Student Dashboard</div>
+      <div className="display-4 bg-warning">
+        Student name: {name}
+      </div>
+      <div className="display-4 bg-info">
+        {messages}
+        Messages:
+      </div>
       <button
         className="btn btn-primary btn-sm "
         onClick={(e) => logout(e)}
@@ -18,7 +53,6 @@ function StudentDashboard({ setAuth }) {
       >
         Logout
       </button>
-      Student Dashboard
     </div>
   );
 }
--- server/routes/studentDashboard.js
+++ server/routes/studentDashboard.js
@@ -11,7 +11,7 @@ router.get("/", authorization, async (req, res) => {
         );
 
         const messages = await pool.query(
-            "SELECT school_message_board.message_content, schools.school_name FROM school_message_board\
+            "SELECT school_message_board.message_content, school_message_board.date_time, schools.school_name FROM school_message_board\
             LEFT JOIN schools ON school_message_board.school_id = schools.school_id\
             LEFT JOIN school_relations ON school_relations.school_id = school_message_board.school_id\
             WHERE school_relations.user_id = $1",
@@ -19,7 +19,8 @@ router.get("/", authorization, async (req, res) => {
         );
 
         const toReturn = {
-            user_name: `${user.rows[0].userName}`
+            user_name: user.rows[0].user_name,
+            messages: JSON.stringify(messages.rows)
         }
 
         res.json(toReturn);
