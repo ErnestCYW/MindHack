c9f204a8c31b17d6193d9518395a074dd57585b2
Timothy Wong <e0559903@u.nus.edu>
2021-07-23 14:56:16 +0800

    Add server skeleton

---
+++ server/db.js
@@ -0,0 +1,12 @@
+// this is a module which helps organize code into manageable parts.
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "password123",
+  host: "localhost",
+  port: 5432,
+  database: "lifehack",
+});
+
+module.exports = pool;
